44a8b5de9471abfcbb75ecf4669588d1052dbeda
Glebuc <gleb567892@gmail.com>
2023-09-11 22:18:51 +0300

    fix connect tailwind css

--- web/frontend/graph_tst_front/src/App.js
+++ web/frontend/graph_tst_front/src/App.js
@@ -1,21 +1,16 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Layout from './components/Layout'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
-class App extends Component {
-  render() {
+import Home from './pages/Home'
+import Dashboard from './pages/Dashboard'
+import Course from './pages/Course'
+import Profile from './pages/Profile'
+import Login from './pages/Login'
+
+function App() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+
     );
-  }
 }
 
 export default App;
